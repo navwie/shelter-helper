dcd612d8e4cceb72a04e215028bfbe748bff1022
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-11 15:35:16 +0300

    Edit sign in front validate with vue

--- resources/js/components/SignIn.vue
+++ resources/js/components/SignIn.vue
@@ -1,14 +1,27 @@
 <template>
     <div class="d-flex col-12 form-signin container justify-content-center align-content-center mt-5">
-        <form>
+        <form action="/signInService" method="get">
             <h1 class="h3 mb-3 fw-normal text-center">Sign in</h1>
             <div class="form-group mt-4">
+                <label for="name">Name</label>
+                <input v-model="name" type="text" :class="'form-control ' + formErrors.name" id="name" name="name" placeholder="Name">
+            </div>
+            <div class="form-group mt-4">
+                <label for="surname">Surname</label>
+                <input v-model="surname" type="text" :class="'form-control ' + formErrors.surname" id="surname" name="surname" placeholder="Surname">
+            </div>
+            <div class="form-group mt-4">
                 <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                <input v-model="email" type="email" :class="'form-control ' + formErrors.email" id="email" name="email" placeholder="name@example.com">
             </div>
             <div class="form-group mt-4">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password">
+                <input v-model="password" type="password" :class="'form-control ' + formErrors.password" id="password" name="password" placeholder="Password">
+            </div>
+            <div class="checkbox mt-2 mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
             </div>
             <div class="form-group text-center mt-4">
                 <button class="btn btn-primary" type="submit">Sign in</button>
@@ -37,12 +50,45 @@
 
 <script>
 export default {
-    name: "SignIn"
+    name: "SignIn",
+    data: () => ({
+        name: "",
+        surname: "",
+        email: "",
+        password: "",
+        formErrors: {}
+    }),
+    watch: {
+        name() {
+            this.formErrors.name = "is-valid";
+            if (this.name.length < 5 || this.name.length > 32) {
+                this.formErrors.name = "is-invalid";
+            }
+        },
+        surname() {
+            this.formErrors.surname = "is-valid";
+            if (this.surname.length < 5 || this.surname.length > 32) {
+                this.formErrors.surname = "is-invalid";
+            }
+        },
+        email() {
+            this.formErrors.email = "is-valid";
+            if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email))) {
+                this.formErrors.email = "is-invalid";
+            }
+        },
+        password() {
+            this.formErrors.password = "is-valid";
+            if ((this.password.length < 5) || !(/[a-zA-Z0-9]/.test(this.password))) {
+                this.formErrors.password = "is-invalid";
+            }
+        },
+    },
 }
 </script>
 
 <style scoped>
     form {
-        margin-top: 7em;
+        margin-top: 3em;
     }
 </style>
