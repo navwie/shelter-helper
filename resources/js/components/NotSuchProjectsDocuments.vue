dfa4496ac83fac9bad19223a28d96305721aa400
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-18 16:11:45 +0300

    Create not such projects at documents page

---
+++ resources/js/components/NotSuchProjectsDocuments.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex col-12 justify-content-center mt-5 text-center">
+        <div class="mt-5">
+            <h4>You haven`t any active projects, please select one or create it</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NotSuchProjectsDocuments"
+}
+</script>
+
+<style scoped>
+    h4 {
+        margin-top: 5em;
+    }
+</style>
