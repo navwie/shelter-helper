057ad066d14c89ea1c959e22f10561bec45364b1
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-18 16:06:34 +0300

    Edit text at not auth page

--- resources/js/components/NotAuth.vue
+++ resources/js/components/NotAuth.vue
@@ -2,7 +2,7 @@
     <div class="d-flex col-12 justify-content-center mt-5 text-center">
         <div class="mt-5">
             <h4>To continue, please <a href="/signUp">sign up</a></h4>
-            <p>If you don`t have account, <a href="/signIn">sign in</a></p>
+            <p>If you don`t have account, please <a href="/signIn">sign in</a></p>
         </div>
     </div>
 </template>
