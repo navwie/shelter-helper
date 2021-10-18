2b87d78ccc3cf9b3318810cffc000abceece4340
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-18 16:12:23 +0300

    Make connection documents with projects at DB

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -9,12 +9,32 @@ class DocumentService
 {
     public static function createDocument(CreateDocumentRequest $request): void
     {
-        DB::table('documents')->insert([
+        $documentId = DB::table('documents')->insertGetId([
             'name' => $request['name'],
             'description' => $request['description'],
             'url' => $request['url'],
             'author_id' => session()->get('userId')
         ]);
+
+        DB::table("projects_documents")
+            ->insert([
+                "project_id" => session()->get('project'),
+                "document_id" => $documentId
+            ]);
+
+
         header("Location: /documents");
     }
+
+    public static function getDocumentsByProject(): string|bool
+    {
+        return json_encode(
+            DB::select('select * from documents where id =
+                              (select document_id from projects_documents where project_id = ?)',
+                [session()->get("project")]
+            )
+        );
+    }
+
+
 }
