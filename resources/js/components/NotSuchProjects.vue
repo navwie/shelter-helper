afdb3b338f8f0cee774e0156ce0f44844c587c7c
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-18 16:04:09 +0300

    Create not such projects page

---
+++ resources/js/components/NotSuchProjects.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex col-12 justify-content-center mt-5 text-center">
+        <div class="mt-5">
+            <h4>You haven`t any projects, please create it</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NotSuchProjects"
+}
+</script>
+
+<style scoped>
+    h4 {
+        margin-top: 5em;
+    }
+</style>
