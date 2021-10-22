fc10529797cbc43ea12cdf0b9acfc2b78a072ecd
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-22 18:06:10 +0300

    Set notification data to documents at template

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.mainLayout')
 
 @section('content')
-    <div id="app" class="container d-flex list-group col-12 align-items-center">
+    <div id="app" class="">
         <documents-list
             :documents="{{ $documents }}"
             :user="{{ $userId }}"
             project="{{ json_encode($project) ?? ""}}"
+            :create_document_notifications="{{ $create_document_notifications }}"
+            :delete_document_notifications="{{ $delete_document_notifications }}"
         />
     </div>
 @endsection
