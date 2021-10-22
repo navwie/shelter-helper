3683d1347c4198a28072d56fa0fa0de347f953a6
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-22 11:18:34 +0300

    Render new notifications at project component

--- resources/js/components/ProjectsList.vue
+++ resources/js/components/ProjectsList.vue
@@ -1,48 +1,63 @@
 <template>
-    <div class="d-flex flex-column align-items-center">
-<!--<form class="col-4 col-lg-auto mb-3 mb-lg-0 me-lg-3 mt-4" action="/searchProjectService">
-            <div class="d-flex flex-row">
-                <input type="search" class="form-control" placeholder="Search..." name="search" aria-label="Search">
-                <button class="btn btn-primary" type="submit">Search</button>
+    <div>
+        <div class="notification">
+            <notify-create-project
+                v-for="notification in this.notifications"
+                :key="notification.id"
+                :id="notification.id"
+                :data="notification.data"
+            />
+        </div>
+
+        <div class="d-flex flex-column align-items-center">
+            <!--<form class="col-4 col-lg-auto mb-3 mb-lg-0 me-lg-3 mt-4" action="/searchProjectService">
+                        <div class="d-flex flex-row">
+                            <input type="search" class="form-control" placeholder="Search..." name="search" aria-label="Search">
+                            <button class="btn btn-primary" type="submit">Search</button>
+                        </div>
+                    </form>-->
+            <div v-if="this.userProjects.length !== 0" class="col-8 mt-5">
+                <h5 class="projects-header">Projects {{ this.userProjects.length }}</h5>
             </div>
-        </form>-->
-        <div v-if="this.userProjects.length !== 0" class="col-8 mt-5">
-            <h5 class="projects-header">Projects {{ this.userProjects.length }}</h5>
+
+            <project
+                v-for="project in this.userProjects"
+                :key="project.index"
+                :id="project.id"
+                :name="project.name"
+                :description="project.description"
+            />
+            <not-such-projects v-if="userProjects.length === 0" />
+
+            <div>
+                <a class="btn btn-outline-dark" href="/createProject">
+                    Create project
+                </a>
+            </div>
         </div>
-        <project
-            v-for="project in this.userProjects"
-            :key="project.index"
-            :id="project.id"
-            :name="project.name"
-            :description="project.description"
-        />
-        <not-such-projects v-if="userProjects.length === 0" />
-        <div>
-            <a class="btn btn-outline-dark" href="/createProject">
-                Create project
-            </a>
-        </div>
+
     </div>
+
 </template>
 
 <script>
 export default {
     name: "Projects",
+    data() {
+        return {
+            show: false
+        }
+    },
     props: {
         projects: [],
         user: "",
-        select: ""
-    },
-    mounted() {
-        Echo.channel('notification')
-            .listen('CreateProjectNotification', (e) => {
-                alert('Message without refresh!');
-            });
+        select: "",
+        notifications: [],
     },
     computed: {
         userProjects: function () {
             return this.projects.filter(project => project.author_id === this.user);
-        }
+        },
         /*selectedProject: function () {
             return this.projects.filter(project => function () {
                 ;
@@ -59,10 +74,16 @@ export default {
 </script>
 
 <style scoped>
+.notification {
+    position: fixed;
+    top: 80px;
+    right: 16px;
+    z-index: 15;
+    font-size: large;
+}
     .projects-header {
         margin-left: 15px;
     }
-
 
     .btn-outline-dark {
         position: fixed;
@@ -73,4 +94,5 @@ export default {
         border-radius: 25px;
         font-weight: 500;
     }
+
 </style>
