42801e150ed72be5146c13e06367ec109cba35eb
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-25 15:35:50 +0300

    Create method that packed cards` data from DB

--- app/Services/BoardService.php
+++ app/Services/BoardService.php
@@ -16,15 +16,29 @@ class BoardService
             'deadline' => $request['deadline']
         ]);
 
-        $boardId = DB::table('boards')
-            ->where('project_id', session()->get('project'))
-            ->value('id');
+        $boardId = self::getBoardByProjectSession();
 
         DB::table("boards_cards")->insert([
             'board_id' => $boardId,
             'card_id' => $cardId,
         ]);
-        
+
         header('Location: /board');
+    }
+
+    public static function getBoardByProjectSession() {
+        return DB::table('boards')
+            ->where('project_id', session()->get('project'))
+            ->value('id');
+    }
+
+    public static function getCardsAtBoard(): string | bool
+    {
+        return json_encode(
+            DB::select('select * from cards where id in
+                              (select card_id from boards_cards where board_id = ?)',
+                [self::getBoardByProjectSession()]
+            )
+        );
     }
 }
