7902f645aac65d6d493bc2680d89e47ea7997407
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-25 12:54:08 +0300

    Create card creating vue component

---
+++ resources/js/components/CreateCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="d-flex col-12 form-signin container justify-content-center align-content-center mt-5">
+        <form action="/createCardService" method="get">
+            <h1 class="h3 mb-3 fw-normal text-center">Create new task</h1>
+            <div class="form-group mt-4">
+                <label for="name"><h5>Name</h5></label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+            </div>
+            <div class="form-group mt-4">
+                <h5>Description</h5>
+                <textarea name="description" id="description" cols="40" rows="5"></textarea>
+            </div>
+            <div class="form-group mt-4">
+                <label for="category"><h5>Category</h5></label>
+                <select name="category" id="category" class="form-select">
+                    <option selected value="backlog">Product backlog</option>
+                </select>
+            </div>
+            <div class="form-group mt-4">
+                <label for="deadline"><h5>Deadline</h5></label>
+                <input type="datetime-local" class="form-control" id="deadline" name="deadline">
+            </div>
+            <div class="form-group text-center mt-3">
+                <a class="btn btn-danger" href="/board">Back</a>
+                <button class="btn btn-primary" type="submit">Create</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CreateCard"
+}
+</script>
+
+<style scoped>
+    form {
+        margin-top: 4em;
+    }
+</style>
