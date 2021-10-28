eb403354fd846a49036c40accc9b2e70287e62fe
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-28 12:30:20 +0300

    Modify link to new route for auto select

--- resources/js/components/Project.vue
+++ resources/js/components/Project.vue
@@ -30,7 +30,7 @@ export default {
         return {
             deleteUrl: "/deleteProjectService/",
             selectUrl: "/selectProject/",
-            infoUrl: "/projectPage/",
+            infoUrl: "/selectAndOpenProjectPage/",
             selected: this.select,
         }
     },
