7f5a7009f64160e467088cc33e968a122da58206
Artem Kuptsov <kuptsov210@gmail.com>
2021-11-04 12:18:17 +0200

    Register edit document vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,7 +48,8 @@ Vue.component('create-card', require('./components/create/CreateCard.vue').defau
 Vue.component('board-card', require('./components/BoardCard.vue').default);
 Vue.component('project-page', require('./components/ProjectPage.vue').default);
 Vue.component('add-user-to-project', require('./components/AddUserToProject.vue').default);
-Vue.component('edit-project', require('./components/EditProject').default);
+Vue.component('edit-project', require('./components/EditProject.vue').default);
+Vue.component('edit-document', require('./components/EditDocument.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
